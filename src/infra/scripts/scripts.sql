d025feba7105584bdd661d1008174802690789ae
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-10-12 15:06:17 -0300

    feat: criação scripts da base de dados

---
+++ src/infra/scripts/scripts.sql
@@ -0,0 +1,37 @@
+CREATE TABLE produtos (
+	id SERIAL NOT NULL PRIMARY KEY,
+	nome VARCHAR (150) NOT NULL,
+	valor decimal NOT null,
+	descricao VARCHAR (300)
+);
+
+CREATE TABLE usuarios (
+	id SERIAL NOT NULL PRIMARY KEY,
+	nome VARCHAR (150) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE compras (
+  id serial PRIMARY KEY,
+  total decimal NOT NULL,
+  idUsuario int NOT null,
+  
+  FOREIGN KEY (idUsuario) REFERENCES usuarios (id)
+);
+
+CREATE TABLE compras (
+  id serial PRIMARY KEY,
+  total decimal NOT NULL,
+  idUsuario integer NOT null,
+  
+  FOREIGN KEY (idUsuario) REFERENCES usuarios(id)
+);
+
+CREATE TABLE items (
+  id serial PRIMARY KEY,
+  idCompra integer NOT NULL,
+  idProduto integer NOT null,
+  
+  FOREIGN KEY (idCompra) REFERENCES compras(id),
+  FOREIGN KEY (idProduto) REFERENCES produtos(id)
+);
